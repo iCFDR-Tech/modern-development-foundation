f49c794d9f45a157263281d6cd40fd85bbd0b79a
Sujay Das <sujay.das@outlook.in>
2025-10-16 11:40:40 +0530

    Fixed the newsletter field and button alignment and made it responsive

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,13 +4,13 @@ import { Input } from "./ui/input";
 export default function Footer() {
   return (
     <div className="px-10 md:px-0 bg-[#000d44] py-20">
-      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-10">
+      <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
         <div>
-          <h1 className="text-2xl md:text-3xl xl:text-4xl font-bold text-white">
+          <h1 className="text-2xl md:text-3xl xl:text-4xl text-center lg:text-start font-bold text-white">
             Subscribe for Newsletter
           </h1>
         </div>
-        <div className="flex gap-10 justify-center md:justify-end ">
+        <div className="flex gap-10 justify-center lg:justify-end ">
            <div>
              <Input className="text-gray-600 rounded-3xl md:w-md" placeholder="Email Address"/>
            </div>

9affd0172bf1e0f23a063767e636c50128827fd8
Sujay Das <sujay.das@outlook.in>
2025-10-16 12:25:14 +0530

    Created the route and skeleton

---
+++ src/app/cancellation-refund/_components/content.jsx
@@ -0,0 +1,5 @@
+export default function Cancellation_Refund(){
+    return (
+        <></>
+    )
+}
---
+++ src/app/cancellation-refund/page.jsx
@@ -0,0 +1,13 @@
+import Cancellation_Refund from "./_components/content";
+
+export default function Page() {
+  return (
+    <>
+      <section>
+        <div className="mt-55 md:mt-40 xl:mt-40 px-8 py-10 md:px-20  xl:px-28">
+            <Cancellation_Refund/>
+        </div>
+      </section>
+    </>
+  );
+}

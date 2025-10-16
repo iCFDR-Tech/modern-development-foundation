cd65f7ea174310446952443cf69f04466870e9b6
Sujay Das <sujay.das@outlook.in>
2025-10-16 12:30:05 +0530

    Created the route and added skeleton

---
+++ src/app/terms-conditions/page.jsx
@@ -0,0 +1,13 @@
+import Terms_Conditions from "./_components/content";
+
+export default function Page() {
+  return (
+    <>
+      <section>
+        <div className="mt-55 md:mt-40 xl:mt-40 px-8 py-10 md:px-20  xl:px-28">
+            <Terms_Conditions/>
+        </div>
+      </section>
+    </>
+  );
+}

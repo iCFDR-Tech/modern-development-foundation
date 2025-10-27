b9f7eedd4806cc7bc432fdcd6792b7e90cbf019a
Sujay Das <sujay.das@outlook.in>
2025-10-27 10:39:46 +0530

    Added options to create build files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // enables static HTML export
+  images: {
+    unoptimized: true, // required if you use <Image>
+  },
+};
 
 export default nextConfig;

5057180d05a4028dd40f7af8b6555e51996aed28
Sujay Das <sujay.das@outlook.in>
2025-10-27 12:45:41 +0530

    Created a not found page

---
+++ src/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound()
+{
+    return <section className="mt-30 py-10 h-80">
+        <div className="container mx-auto h-full flex justify-center items-center">
+            <h1 className=" text-lg md:text-2xl xl:text-3xl font-bold">404 Page Not Found !!</h1>
+        </div>
+    </section>
+}
